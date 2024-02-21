61c2491f02d390a20dff0b7cddd6cc8618caac37
Vladyslav Savchyn <vsavchyn2005@gmail.com>
2024-02-21 14:54:44 -0500

    fix: can build + run

--- blockchain.cpp
+++ blockchain.cpp
@@ -1,20 +1,22 @@
 #include "blockchain.hpp"
+#include <string>
 
-Block::Block(const uint64_t& id, const std::string& prev_hash, const std::string& details)        
-    : 
-    id_(id), 
-    prev_hash_(prev_hash), 
-    tran_details_(details) 
-    {
-        tran_time_ = std::chrono::system_clock::now();
-        auto out   = mine(id, prev_hash, details, std::chrono::system_clock::to_time_t(tran_time_));
-        tran_hash_ = out.first;
-        nonce_     = out.second;
-    }
+Block::Block(const uint64_t& id, const std::string& prev_hash,
+             const std::string& details)
+    : id_(id), prev_hash_(prev_hash), tran_details_(details) {
+    tran_time_ =
+        std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    auto out = mine(id, prev_hash, details, tran_time_);
+    tran_hash_ = out.first;
+    nonce_ = out.second;
+}
 
-std::string hash(const uint64_t& id, const uint64_t& nonce, const std::string& prev_hash, const std::string& details, uint64_t time_point)
-{
-    std::string coded = std::format("{}{}{}{}{}", id, prev_hash, time_point, details, nonce);
+std::string Block::hash(const uint64_t& id, const uint64_t& nonce,
+                        const std::string& prev_hash,
+                        const std::string& details, uint64_t time_point) {
+    std::string coded = std::to_string(id) + prev_hash +
+                        std::to_string(time_point) + details +
+                        std::to_string(nonce);
     std::stringstream tmp_ss;
     unsigned char hash[SHA256_DIGEST_LENGTH];
     SHA256_CTX sha;
@@ -22,28 +24,30 @@ std::string hash(const uint64_t& id, const uint64_t& nonce, const std::string& p
     SHA256_Update(&sha, coded.c_str(), coded.size());
     SHA256_Final(hash, &sha);
 
-    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++)
-    {
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
         tmp_ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
     }
 
     return tmp_ss.str();
 }
 
-std::pair<std::string, uint64_t> mine(const uint64_t& id, const std::string& prev_hash, const std::string& details, uint64_t time_point)
-{
+std::pair<std::string, uint64_t> Block::mine(const uint64_t& id,
+                                             const std::string& prev_hash,
+                                             const std::string& details,
+                                             uint64_t time_point) {
     uint64_t nonce = 0;
 
     // need to finish this
-    for(;;)
-    {
+    for (;;) {
         std::string hash_sha = hash(id, nonce, prev_hash, details, time_point);
         return std::pair<std::string, uint64_t>(hash_sha, nonce);
     }
 }
 
-std::string Block::toString() const
-{
-    std::string retStr = std::format("{}:{}:{}:{}:{}:{}", id_, nonce_, std::chrono::system_clock::to_time_t(tran_time_), prev_hash_, tran_details_, tran_hash_);
+std::string Block::toString() const {
+    std::string retStr = std::to_string(id_) + "|" + prev_hash_ + "|" +
+                         std::to_string(tran_time_) + "|" + tran_details_ +
+                         "|" + std::to_string(nonce_) + "|" + tran_hash_;
+
     return retStr;
 }
--- blockchain.hpp
+++ blockchain.hpp
@@ -1,25 +1,29 @@
-#include <list>
-#include <iomanip>
-#include <string>
-#include <sstream>
 #include <chrono>
-// format is used only in C++20 and later, then why the hell this shit doesn't wrok with stock complier on mac?
-#include <format>
-// include doesn't unless vcpkg works
+#include <iomanip>
+#include <list>
 #include <openssl/sha.h>
+#include <sstream>
+#include <string>
 
 class Block {
     uint64_t id_;
     uint64_t nonce_;
     std::string prev_hash_;
     std::string tran_details_;
-    std::chrono::time_point<std::chrono::system_clock> tran_time_;
+    uint64_t tran_time_;
     std::string tran_hash_;
-    
-    std::string hash(const uint64_t& id, const uint64_t& nonce, const std::string& prev_hash, const std::string& details, uint64_t time_point);
-    std::pair<std::string, uint64_t> mine(const uint64_t& id, const std::string& prev_hash, const std::string& details, uint64_t time_point);
+
+    std::string hash(const uint64_t& id, const uint64_t& nonce,
+                     const std::string& prev_hash, const std::string& details,
+                     uint64_t time_point);
+    std::pair<std::string, uint64_t> mine(const uint64_t& id,
+                                          const std::string& prev_hash,
+                                          const std::string& details,
+                                          uint64_t time_point);
+
 public:
-    Block(const uint64_t& id, const std::string& prev_hash, const std::string& details);
+    Block(const uint64_t& id, const std::string& prev_hash,
+          const std::string& details);
 
     std::string toString() const;
 };
