d81d7829f5ba2462e4a6f17fad6b1f5c1ee74b2b
Vladyslav Savchyn <vsavchyn2005@gmail.com>
2024-02-21 20:03:21 -0500

    fix: finished

--- blockchain.cpp
+++ blockchain.cpp
@@ -1,5 +1,8 @@
 #include "blockchain.hpp"
+#include <cstdint>
 #include <string>
+
+const std::string DIFFICULTY_PREFIX = "00000";
 
 Block::Block(const uint64_t& id, const std::string& prev_hash,
              const std::string& details)
@@ -37,11 +40,28 @@ std::pair<std::string, uint64_t> Block::mine(const uint64_t& id,
                                              uint64_t time_point) {
     uint64_t nonce = 0;
 
-    // need to finish this
     for (;;) {
         std::string hash_sha = hash(id, nonce, prev_hash, details, time_point);
-        return std::pair<std::string, uint64_t>(hash_sha, nonce);
+
+        if (hash_sha.starts_with(DIFFICULTY_PREFIX)) {
+            std::cout << "Block " << id << " was mined!\n";
+            return std::pair<std::string, uint64_t>(hash_sha, nonce);
+        }
+
+        nonce++;
     }
+}
+
+const uint64_t& Block::getId() const {
+    return id_;
+}
+
+const std::string& Block::getHash() const {
+    return tran_hash_;
+}
+
+const std::string& Block::getPrevHash() const {
+    return prev_hash_;
 }
 
 std::string Block::toString() const {
@@ -50,4 +70,63 @@ std::string Block::toString() const {
                          "|" + std::to_string(nonce_) + "|" + tran_hash_;
 
     return retStr;
+}
+
+inline Block Blockchain::createGenesis() const {
+    return Block(0, "0000", "genesis");
+}
+
+Blockchain::Blockchain() {
+    chain.push_back(this->createGenesis());
+}
+
+bool Blockchain::isBlockValid(const Block& currBlock,
+                              const Block& prevBlock) const {
+    if (currBlock.getId() == prevBlock.getId() + 1 &&
+        currBlock.getPrevHash() == prevBlock.getHash()) {
+        std::cout << "Block " << currBlock.getId() << " is valid\n";
+        return true;
+    } else {
+        std::cout << "Block " << currBlock.getId() << " is invalid\n";
+        return false;
+    }
+}
+
+bool Blockchain::isChainValid() const {
+    auto previousBlock = chain.begin();
+    auto currentBlock = ++chain.begin();
+
+    std::cout << "Checking validity of chain:\n";
+
+    for (uint64_t i = 1; i < chain.size(); i++) {
+        if (!isBlockValid(*currentBlock, *previousBlock)) {
+            return false;
+        }
+
+        ++previousBlock;
+        ++currentBlock;
+    }
+
+    return true;
+}
+
+bool Blockchain::tryAddNewBlock(const Block& block) {
+    Block last = this->getLast();
+
+    if (isBlockValid(block, last)) {
+        chain.push_back(block);
+        std::cout << "Block was succesfully added to blockchain\n";
+        return true;
+    } else {
+        std::cout << "Can\'t add block " << block.getId() << " - invalid\n";
+        return false;
+    }
+}
+
+const Block& Blockchain::getLast() const {
+    return chain.back();
+}
+
+const uint64_t Blockchain::getChainLen() const {
+    return chain.size();
 }
